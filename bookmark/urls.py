ffd4756d6572020874f3a81e70c9f310bfbf2963
Youngbai You <nawhizz@gmail.com>
2019-02-26 12:26:50 +0900

    first commit

---
+++ bookmark/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from bookmark.views import BookmarkLV, BookmarkDV
+
+
+urlpatterns = [
+    path('', BookmarkLV.as_view(), name='index'),
+    path('<int:pk>/', BookmarkDV.as_view(), name='detail'),
+]
